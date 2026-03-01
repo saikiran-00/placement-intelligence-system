2ea400caf0d4e6fa6112a739aae9ff3b699c072e
Your Name <your@email.com>
2026-03-01 21:19:20 +0530

    fix Railway dynamic PORT issue

--- backend/server.js
+++ backend/server.js
@@ -168,7 +168,7 @@ mongoose.connect(process.env.MONGO_URI)
   .catch(err => console.log(err));
 
 // ------------------- START SERVER -------------------
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
